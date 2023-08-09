ada2e6db8e565ce42c837881374a4220b85ba513
Shogo Yamato <yamash.yamash@gmail.com>
2023-08-09 22:52:14 +0900

    feat: reproduction code

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import("next").NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/api/graphql",
+        destination:
+          "https://swapi-graphql.netlify.app/.netlify/functions/index",
+      },
+    ];
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,46 @@
+import { useCallback, useEffect, useState } from "react";
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  const [data, setData] = useState();
+  const fetchData = useCallback(async () => {
+    const data = await (
+      await fetch("/api/graphql", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          query: `query AllPlanets($first: Int) {
+  allPlanets(first: $first) {
+    edges {
+      cursor
+      node {
+        created
+        id
+        name
+      }
+    }
+    pageInfo {
+      endCursor
+      hasNextPage
+      hasPreviousPage
+      startCursor
+    }
+    totalCount
+  }
+}`,
+          variables: {
+            first: 20,
+          },
+        }),
+      })
+    ).json();
+    setData(data);
+  }, []);
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return <div>{JSON.stringify(data)}</div>;
 }
